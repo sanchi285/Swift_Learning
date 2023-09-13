aae8e50f9d95b490fd5b6377069c29d71df55f9e
sanchi285 <bismoy.sanchi@gmail.com>
2023-09-13 06:11:06 +0600

    Test swift code has been created

---
+++ test.swift
@@ -0,0 +1,14 @@
+/*
+export PATH=~/swift-5.8.1-RELEASE-ubuntu22.04/usr/bin:"$PATH"
+*/
+import Foundation
+let myName = "Vandad"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+names.append("Bar")
+print("Hello Sanchi")
+print(names)
